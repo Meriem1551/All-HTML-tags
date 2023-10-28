7fcfc27f6e743bd8b7339634ea74558cf56dcb5a
Meriem1551 <meriem200409@gmail.com>
2023-10-28 20:04:10 +0200

    Form and validation

---
+++ app.js
@@ -0,0 +1,9 @@
+const email = document.getElementById("mail");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
